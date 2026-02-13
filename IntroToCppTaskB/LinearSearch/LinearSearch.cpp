da264215f42edb029b323b523be618cfc5b6e928
bevenk101@gmail.com <bevenk101@gmail.com>
2026-02-13 12:24:22 +1100

    Linear search part 2

--- IntroToCppTaskB/LinearSearch/LinearSearch.cpp
+++ IntroToCppTaskB/LinearSearch/LinearSearch.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <cassert>
 
-int LinearSearch(int TargetNum, int InputArray[], int ArrayLength)
+int LinearSearch(int TargetNum, int InputArray[], int ArrayLength)// loops through an array to find a taget number and returns the index of the number within the array
 {
 	for (int i = 0; i < ArrayLength; i++)
 	{
@@ -14,5 +15,13 @@ int LinearSearch(int TargetNum, int InputArray[], int ArrayLength)
 
 int main()
 {
+	int NewArray[]{ 67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29 };
+	const int Length = sizeof(NewArray) / sizeof(NewArray[0]);
+
+	
+	assert(LinearSearch(3, NewArray, Length) == 2);
+	assert(LinearSearch(7, NewArray, Length) == 11);
+	assert(LinearSearch(67, NewArray, Length) == 0);
+	assert(LinearSearch(88, NewArray, Length) == -1);
 
 }
